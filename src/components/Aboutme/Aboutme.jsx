dedffd0b9a29fe401da40d5fec7847de57b30d63
dudegangwar <dudegangwar@gmail.com>
2024-02-17 21:21:20 +0530

    added tech stack images

--- src/components/Aboutme/Aboutme.jsx
+++ src/components/Aboutme/Aboutme.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-
 const Aboutme = () => {
   return (
     <div>
@@ -14,25 +13,205 @@ const Aboutme = () => {
           src="https://avatars.githubusercontent.com/u/86113409?v=4"
         />
       </div>
+      <div class="gallery">
+        <img src="../src/assets/anuj.jpg" alt="Image 1" />
+        <img src="../src/assets/anuj.jpg" alt="Image 2" />
+        <img src="../src/assets/anuj.jpg" alt="Image 3" />
+      </div>
       <div className="mt-4">
         <h1 className="underline underline-offset-4 text-lg">About Me</h1>
-        <p>I am Anuj Kumar, a dedicated and passionate Data Science enthusiast with a keen interest in leveraging technology to solve real-world problems. Currently pursuing a Bachelor's degree in Data Science and Applications from the prestigious Indian Institute of Technology, Madras, I have a solid foundation in computer science, complemented by practical experience and a strong academic background.</p>
+        <p>
+          I am Anuj Kumar, a dedicated and passionate Data Science enthusiast
+          with a keen interest in leveraging technology to solve real-world
+          problems. Currently pursuing a Bachelor's degree in Data Science and
+          Applications from the prestigious Indian Institute of Technology,
+          Madras, I have a solid foundation in computer science, complemented by
+          practical experience and a strong academic background.
+        </p>
       </div>
       <div className="mt-4">
         <h1 className="underline underline-offset-4 text-lg">Education</h1>
-        <p>My educational journey began with a Diploma in Computer Science and Engineering from Government Polytechnic, Shahjahanpur, where I achieved a commendable CGPA of 7.5. Prior to that, I completed my intermediate education from SV Inter College, Bareilly, and high school from Sanatan Dharam HSS School, Bareilly, demonstrating consistent academic excellence.</p>
+        <table>
+          <tr>
+            <th>Education Level</th>
+            <th>Institution</th>
+            <th>Duration</th>
+            <th>CGPA/Percentage</th>
+          </tr>
+          <tr>
+            <td>Bachelor's in Data Science and Applications</td>
+            <td>Indian Institute of Technology, Madras</td>
+            <td>Jan 2021 - Present</td>
+            <td>7 (CGPA)</td>
+          </tr>
+          <tr>
+            <td>Diploma in Computer Science and Engineering</td>
+            <td>Government Polytechnic, Shahjahanpur</td>
+            <td>2017 - 2020</td>
+            <td>7.5 (CGPA)</td>
+          </tr>
+          <tr>
+            <td>Intermediate</td>
+            <td>SV Inter College, Bareilly</td>
+            <td>2016 - 2017</td>
+            <td>61.4%</td>
+          </tr>
+          <tr>
+            <td>High School</td>
+            <td>Sanatan Dharam HSS School, Bareilly</td>
+            <td>2014 - 2015</td>
+            <td>79%</td>
+          </tr>
+        </table>
       </div>
       <div className="mt-4">
-        <h1 className="underline underline-offset-4 text-lg">Technical Skills</h1>
-        <p>Proficient in a wide array of technical skills, I specialize in Data Science, with expertise in Machine Learning, Deep Learning, and Natural Language Processing. My programming prowess includes languages such as Python, JavaScript, HTML/CSS, and SQL. Additionally, I am well-versed in frameworks like Vue.js, Flask, and WordPress, along with a range of libraries including Scikit-learn, TensorFlow, PyTorch, and NLTK.</p>
+        <h1 className="underline underline-offset-4 text-lg">
+          Technical Skills
+        </h1>
+        <p>I have become confident using the following technologies.</p>
+        <div className="flex items-center justify-between bg-white p-4 rounded-2xl m-2">
+          <img
+            className="w-16 h-16"
+            src="https://raw.githubusercontent.com/tomchen/stack-icons/634d5c036a2a7ca0115c94ab2ce86c7e79e01e13/logos/html-5.svg"
+          />
+          <img
+            className="w-16 h-16"
+            src="https://raw.githubusercontent.com/tomchen/stack-icons/634d5c036a2a7ca0115c94ab2ce86c7e79e01e13/logos/css-3.svg"
+          />
+          <img
+            className="w-16 h-16"
+            src="https://raw.githubusercontent.com/tomchen/stack-icons/634d5c036a2a7ca0115c94ab2ce86c7e79e01e13/logos/javascript.svg"
+          />
+          <img
+            className="w-16 h-16"
+            src="https://raw.githubusercontent.com/tomchen/stack-icons/634d5c036a2a7ca0115c94ab2ce86c7e79e01e13/logos/bootstrap.svg"
+          />
+          <img
+            className="w-16 h-16"
+            src="https://raw.githubusercontent.com/tomchen/stack-icons/634d5c036a2a7ca0115c94ab2ce86c7e79e01e13/logos/react.svg"
+          />
+          <img
+            className="w-16 h-16"
+            src="https://raw.githubusercontent.com/tomchen/stack-icons/634d5c036a2a7ca0115c94ab2ce86c7e79e01e13/logos/vue.svg"
+          />
+          <img
+            className="w-16 h-16"
+            src="https://raw.githubusercontent.com/tomchen/stack-icons/634d5c036a2a7ca0115c94ab2ce86c7e79e01e13/logos/python.svg"
+          />
+          <img
+            className="w-16 h-16"
+            src="https://raw.githubusercontent.com/tomchen/stack-icons/634d5c036a2a7ca0115c94ab2ce86c7e79e01e13/logos/sqlite.svg"
+          />
+        </div>
+        <div className="flex items-center justify-between bg-white p-4 rounded-2xl m-2">
+          <img
+            className="w-16 h-16"
+            src="https://seeklogo.com/images/A/ahrefs-logo-F4E31FB5C2-seeklogo.com.png"
+          />
+          <img
+            className="w-16 h-16"
+            src="https://cdn.freelogovectors.net/wp-content/uploads/2023/10/semrush-icon-freelogovectors.net_.png"
+          />
+          <img
+            className="w-16 h-16"
+            src="https://raw.githubusercontent.com/tomchen/stack-icons/634d5c036a2a7ca0115c94ab2ce86c7e79e01e13/logos/flask.svg"
+          />
+          <img
+            className="w-16 h-16"
+            src="https://raw.githubusercontent.com/tomchen/stack-icons/634d5c036a2a7ca0115c94ab2ce86c7e79e01e13/logos/redis.svg"
+          />
+          <img
+            className="w-16 h-16"
+            src="https://www.vectorlogo.zone/logos/wordpress/wordpress-tile.svg"
+          />
+          <img
+            className="w-16 h-16"
+            src="https://elementor.com/marketing/wp-content/uploads/2021/10/Elementor-Logo-Symbol-Red.svg"
+          />
+          <img
+            className="w-16 h-16"
+            src="https://www.drupal.org/files/project-images/gutenberg_pattern_drupal_720_0_0.png"
+          />
+        </div>
+        <p>
+          Proficient in a wide array of technical skills, I specialize in Data
+          Science, with expertise in Machine Learning, Deep Learning, and
+          Natural Language Processing. My programming prowess includes languages
+          such as Python, JavaScript, HTML/CSS, and SQL. Additionally, I am
+          well-versed in frameworks like Vue.js, Flask, and WordPress, along
+          with a range of libraries including Scikit-learn, TensorFlow, PyTorch,
+          and NLTK.
+        </p>
       </div>
       <div className="mt-4">
         <h1 className="underline underline-offset-4 text-lg">Experience</h1>
-        <p>As an SEO Analyst at Aphex Media, I honed my skills in developing and implementing effective SEO strategies to improve website rankings and increase organic traffic. Utilizing tools such as Google Analytics, SEMrush, and Ahrefs, I conducted comprehensive keyword research, tracked rankings, and identified opportunities for optimization.</p>
+        <p>
+          As an SEO Analyst at Aphex Media, I honed my skills in developing and
+          implementing effective SEO strategies to improve website rankings and
+          increase organic traffic. Utilizing tools such as Google Analytics,
+          SEMrush, and Ahrefs, I conducted comprehensive keyword research,
+          tracked rankings, and identified opportunities for optimization.
+        </p>
       </div>
       <div className="mt-4">
         <h1 className="underline underline-offset-4 text-lg">Projects</h1>
-        <p>I have a proven track record of successfully delivering impactful projects. One notable project is the "E-commerce Shopper’s Behaviour Understanding," where I developed a predictive model to analyze user session data and predict purchase behavior with an accuracy of 62%. Another innovative project is the creation of a "Chatbot to give answers from PDFs," showcasing my expertise in Natural Language Processing. Additionally, I have contributed to the development of the "Tasker App," a task management application designed to enhance productivity and workflow efficiency. Moreover, my experience in "Business Analysis of Smartphone Shop" demonstrates my ability to solve real-world problems using data-driven insights and analytics tools.</p>
+        <div class="project-container">
+          <div class="project">
+            <h2>E-commerce Shopper’s Behaviour Understanding</h2>
+            <p>
+              Developed a predictive model to determine purchase behavior based
+              on user session data.
+            </p>
+            <p>Accuracy achieved: 62%</p>
+            <a href="#" class="project-link">
+              View Project
+            </a>
+          </div>
+
+          <div class="project">
+            <h2>Chatbot to give answers from PDFs</h2>
+            <p>Created a chatbot to extract answers from uploaded PDF files.</p>
+            <a href="#" class="project-link">
+              View Project
+            </a>
+          </div>
+
+          <div class="project">
+            <h2>Tasker App</h2>
+            <p>
+              Designed a task management app for efficient organization and
+              productivity.
+            </p>
+            <a href="#" class="project-link">
+              View Project
+            </a>
+          </div>
+
+          <div class="project">
+            <h2>Business Analysis of Smartphone Shop</h2>
+            <p>
+              Utilized data analysis and visualization to optimize pricing
+              strategies and identify growth opportunities.
+            </p>
+            <a href="#" class="project-link">
+              View Project
+            </a>
+          </div>
+        </div>
+        <p>
+          I have a proven track record of successfully delivering impactful
+          projects. One notable project is the "E-commerce Shopper’s Behaviour
+          Understanding," where I developed a predictive model to analyze user
+          session data and predict purchase behavior with an accuracy of 62%.
+          Another innovative project is the creation of a "Chatbot to give
+          answers from PDFs," showcasing my expertise in Natural Language
+          Processing. Additionally, I have contributed to the development of the
+          "Tasker App," a task management application designed to enhance
+          productivity and workflow efficiency. Moreover, my experience in
+          "Business Analysis of Smartphone Shop" demonstrates my ability to
+          solve real-world problems using data-driven insights and analytics
+          tools.
+        </p>
       </div>
     </div>
   );
